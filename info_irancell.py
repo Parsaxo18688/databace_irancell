fd201987238f8951931052ab352f1932346ba371
elf <87319528+elf-cyber@users.noreply.github.com>
2021-09-17 23:13:09 +0430

    Update info_irancell.py

--- info_irancell.py
+++ info_irancell.py
@@ -6,7 +6,9 @@ userID = input(colorama.Fore.GREEN+'enter the number {930,933,935,936,937,938,93
 response = requests.get('https://ziroapi.xyz/Apis/irancell/?phone='+userID)
 
 response.encoding = 'utf-8'
-response.text
-print(colorama.Fore.MAGENTA+'  {   "@elf_security_cyber" '+response.text+'\n'+
+response.json()
+print(colorama.Fore.CYAN+'{ "craeted by : @e_l_f_6_6_6"'+json.dumps(response.json(), indent=4, separators=(" , ", " ==> :  ")))
+print('''
       
-      ''' "@e_l_f_6_6_6" } ''')
+      
+         my channel : @elf-security_cyber       }   ''')
