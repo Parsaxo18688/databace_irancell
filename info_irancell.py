b15ccc9caed1e70a0e390694a6d82b06f4014758
elf <87319528+elf-cyber@users.noreply.github.com>
2021-09-17 23:14:32 +0430

    Update info_irancell.py

--- info_irancell.py
+++ info_irancell.py
@@ -1,13 +1,64 @@
 import requests
 import colorama
-userID = input(colorama.Fore.GREEN+'enter the number {930,933,935,936,937,938,939} : ')
+
+print(colorama.Fore.LIGHTYELLOW_EX+"""
+                                # elf #
+                           .7KBQBBBBBBBBBIr                           
+                        i5RQQP1ri..   ..i7IbQQM2i                      
+                    rQBBDY.                   .JgBBMr                  
+                 rBQB7                             vBBBi               
+               PBBr                                   vBQI             
+             ZBD.                                       .QBX           
+           IBg                                             BBv         
+          BB.                                               .BB        
+        vBY      qq.       ...:r::i.::i::i:..        :dI      XBi      
+       DB     :BBR      ..: .:.  :  : ..  ::....       BBB.    .B2     
+      BB   g:1BBi     .r:   .   ..  :  :.   .   :i.     7BQ7rd   Bb    
+     gB   BE.BIi:   .:. ..:i.   i   i   :   .i:.. .:.   i:PB BB   BI   
+    YB   BB :iIB   :.    ..  ..:: dd7BQ:.:..  ..    .:  .BJi. Qd   Bi  
+    B  B BQ QBB   i     .:     :  BR XBB .     :.     i   BBD BB.B :B  
+   BM PB BQBU:   :      i      i    :BM  i      i      i   iXBBB Bv BB 
+  .B  BB Qu P7  i....  :.     ..    X    :      ..  ....i  u1 qg BB  B 
+  BB  BB  rBB  i    ...i....  :.   .5    :. .....i...    :  BBi .BB  Bj
+  B   BB DBZ  .:      ..   .i...# elf #...: .    :       :   BBI.BZ  iB
+ jQ Y: BQB..  :       :.      :           .      ..      .:   :BMB Y: B
+ BB MB RB  B  i       :       .   rEXEi   .       :       :  B  BS B1 B
+ BZ vB: : BP  i.......:        1B .QBd  Bv       .:.......:  BB r vB: B
+ BM  BB  BB   i . . . :  .:rUZBBB  rB:  BBBE1i:.  : ... . i   BB .BB  B
+ BB  KBgiBY   :        rBBBBBBQB.   Q   iBBBBBBBBi        :   KB:BBL  B
+ vB L YBBB Qi :        BBBBQBBBB:  5B7  7BBBBBBBBZ       .: LK BQQi r B
+  B rB  BR Bd  :       BQBQBQBBBQ  MQS  QBQBBBBBBB       :  BB BB  B.:B
+  QQ BB. : QB  :    . :BBBBBBBBBB7 bBs 5BBBBBBBBBB. .   ..  BB i :BB BY
+   B  BBB  BQ i :...  :BBBBBQBBBBB:KBJiBBBBBBBBBBB   ..:. r BB  BBQ  B 
+   BB  uBBiBB Bj .    PBBQBBBBBQBBBBBQBBBBBBBQBQBBJ    . dB BRrBB7  BQ 
+    B. 7 vBBQ rB: .   QBBBBBBBBBBBBBQBBBBBBBBBBBBBZ   . 7B..BBBr 7 7B  
+    7B iB.  K5 BB     BQBBBBBQBQBBBBBBBQBBBBBBBBBBB     BB D1  iB. Q:  
+     bQ .BBP:: YBd 5  BBQBBBBBBBQBQBBBQBBBBBQBBBBBB  X BBi ::gBB  BJ   
+      gB  rBBBBrZB.uB7BBBBBBBBBQBBBBBQBBBQBBBBBBBQBrBi:QqrBBBQ:  BI    
+       KB.  ivqBMBB 1BBBBBBBBBBBBBBBBBBBBBBBBBBBBBQB7 BBMBSvi  :BY     
+        rQ5 .u7:. :r .KQBBBBBBBBBQBQBBBBBBBBBBBQBQS  7: .:vJ  DB:      
+          QB  iBBBBBBBBBQKrQQBBBBBBBBBBBBBBBQZrPQBBBBBBBBQ: .BB        
+           7Bg   :i:..    iQBBBBBBBQBQBBBBBBBRi    ..:r.   BBi         
+             XBR  .2ZQBBBBBBQBBBBBBBBBBBBBBBQBBBBBBQE1. .BBu           
+               uBQi  .r7i  BBQBBBBBQBBBBBBBBBQ .rri.  rBBv             
+                 :gBgi     BQBBBBBBBQBBBBBBBBB     rMBZ.               
+                    idBBgrJBBBBBBBBBBBBBBBBBBB77QBBP:                  
+                        .rv1XZMQQBBBBBBBQQgZSu7i            
+                                  bay     
+      
+      
+      
+      
+ """)
+
+number = input(colorama.Fore.GREEN+'enter the number {930,933,935,936,937,938,939} : ')
  
 
-response = requests.get('https://ziroapi.xyz/Apis/irancell/?phone='+userID)
+a= requests.get('https://ziroapi.xyz/Apis/irancell/?phone='+number)
 
-response.encoding = 'utf-8'
-response.json()
-print(colorama.Fore.CYAN+'{ "craeted by : @e_l_f_6_6_6"'+json.dumps(response.json(), indent=4, separators=(" , ", " ==> :  ")))
+a.encoding = 'utf-8'
+a.json()
+print(colorama.Fore.CYAN+'{ "craeted by : @e_l_f_6_6_6"'+json.dumps(a.json(), indent=4, separators=(" , ", " ==> :  ")))
 print('''
       
       
